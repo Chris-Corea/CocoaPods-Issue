3cb84824a6ec9c994750e13ac623ca6f197ba32b
Chris Corea <christopherc@juul.com>
2018-08-30 11:46:43 -0700

    Initial Commit

---
+++ CocoaPods_TestProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CocoaPods_TestProject
+//
+//  Created by Christopher Corea on 8/30/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
